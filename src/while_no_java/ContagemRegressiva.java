d4f752783ad64d933390c01bd89a83241aa92f31
yukimura-kyoto <259417315+yukimura-kyoto@users.noreply.github.com>
2026-04-15 14:06:15 -0300

    feat: ContagemRegressiva

---
+++ src/while_no_java/ContagemRegressiva.java
@@ -0,0 +1,12 @@
+package while_no_java;
+
+public class ContagemRegressiva {
+    static void main() {
+        int i=10;
+
+        while(i>=0){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
